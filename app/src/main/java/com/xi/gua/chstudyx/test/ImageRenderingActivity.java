8fdc1a4d82ada8cc4a34aa9d6dc52598250338dd
ch-617 <547823828@qq.com>
2020-06-09 11:15:01 +0800

    新增滤镜叠加效果

--- app/src/main/java/com/xi/gua/chstudyx/test/ImageRenderingActivity.java
+++ app/src/main/java/com/xi/gua/chstudyx/test/ImageRenderingActivity.java
@@ -3,6 +3,7 @@ package com.xi.gua.chstudyx.test;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -11,8 +12,8 @@ import com.xi.gua.chstudyx.R;
 
 import jp.co.cyberagent.android.gpuimage.GPUImage;
 import jp.co.cyberagent.android.gpuimage.GPUImageBrightnessFilter;
+import jp.co.cyberagent.android.gpuimage.GPUImageBulgeDistortionFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageContrastFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageFalseColorFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageGrayscaleFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageSaturationFilter;
 
@@ -43,6 +44,7 @@ public class ImageRenderingActivity extends BaseActivity {
         tv_bhd = findViewById(R.id.tv_bhd);
         tv_ld = findViewById(R.id.tv_ld);
         tv_dbd = findViewById(R.id.tv_dbd);
+
 
         sb_bhd.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -100,21 +102,74 @@ public class ImageRenderingActivity extends BaseActivity {
         });
     }
 
-    public Bitmap getGPUinageFromAssets(int progress,Bitmap bitmap, int flag){
-        // 使用GPUImage处理图像
-        gpuImage.setImage(bitmap);
+    private float saturation = 1.0f;// 饱和度saturation: The degree of saturation or desaturation to apply to the image (0.0 - 2.0, with 1.0 as the default)
+    private float brighness =  0.0f;// 亮度brightness value ranges from -1.0 to 1.0, with 0.0 as the normal level
+    private float contrast = 1.0f;  // 对比度contrast value ranges from 0.0 to 4.0, with 1.0 as the normal level
+
+    public Bitmap getGPUinageFromAssets(int progress,Bitmap mBitmap, int flag){
         if (flag == 1) {
             //设置饱和度
-            gpuImage.setFilter(new GPUImageSaturationFilter(progress * 0.02f));
+            saturation = progress * 0.02f;
+//            gpuImage.setFilter(new GPUImageSaturationFilter(saturation));
         } else if (flag == 2) {
-            //要实现亮度的调整仅需将Filter改为GPUImageBrightnessFilter，并且在设置progress的时候在区间0-1之间设置便可
-            gpuImage.setFilter(new GPUImageBrightnessFilter(progress * 0.02f - 1));
+            //设置亮度
+            brighness = progress * 0.02f - 1;
+//            gpuImage.setFilter(new GPUImageBrightnessFilter(brighness));
         } else if (flag == 3) {
             // 对比度
-            gpuImage.setFilter(new GPUImageContrastFilter(progress * 0.04f));
+            contrast = progress * 0.04f;
+//            gpuImage.setFilter(new GPUImageContrastFilter(contrast));
         }
+        // 使用GPUImage处理图像
+        Bitmap bitmap = mBitmap;
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageSaturationFilter(saturation));
         bitmap = gpuImage.getBitmapWithFilterApplied();
+
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageBrightnessFilter(brighness));
+        bitmap = gpuImage.getBitmapWithFilterApplied();
+
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageContrastFilter(contrast));
+        bitmap = gpuImage.getBitmapWithFilterApplied();
+
         return bitmap;
 
+    }
+
+    public void btn_gray(View view) {
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageGrayscaleFilter());
+        Bitmap bitmap = gpuImage.getBitmapWithFilterApplied();
+        iv_forest.setImageBitmap(bitmap);
+    }
+
+    public void btn_fish_eye(View view) {
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageBulgeDistortionFilter());
+        Bitmap bitmap = gpuImage.getBitmapWithFilterApplied();
+        iv_forest.setImageBitmap(bitmap);
+    }
+
+    public void btn_gray_and_fish_eye(View view) {
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageGrayscaleFilter());
+        Bitmap bitmap1 = gpuImage.getBitmapWithFilterApplied();
+
+        gpuImage.setImage(bitmap1);
+        gpuImage.setFilter(new GPUImageBulgeDistortionFilter());
+        Bitmap bitmap2 = gpuImage.getBitmapWithFilterApplied();
+        iv_forest.setImageBitmap(bitmap2);
+    }
+
+
+    public void btn_reset(View view) {
+        sb_bhd.setProgress(50);
+        sb_ld.setProgress(50);
+        sb_dbd.setProgress(25);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_forest);
+        iv_forest.setImageBitmap(bitmap);
     }
 }
